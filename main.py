d420c33774f16a81739de662e926c13c65944d0b
Dekoder-py <dekod3r.py@gmail.com>
2025-09-17 21:28:35 +1200

    add initial logic for game loop

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+from rich import print
+from random import choice
+import os
+
+WORD_LIST = ["summer", "hack"]
+
+def clear_screen():
+    if os.name == 'nt': # windows
+        _ = os.system('cls')
+    else: # macOS and linux
+        _ = os.system('clear')
+
+def game():
+    word = choice(WORD_LIST)
+    guesses = 0
+    print(f"[b]The word is {len(word)} letters long.[/b]")
+    while guesses < 10:
+        guess = input("Guess: ").lower().strip()
+        if guess == word:
+            print("[green]You Win![/green]")
+            quit(0)
+        else:
+            print("Wrong.")
+            guesses += 1
+            print(f"You have {10 - guesses} guesses remaining.")
+            clear_screen()
+
 def main():
-    print("Hello from hangman!")
+    clear_screen()
+    print("Welcome to hangman! Guess the word in less than 10 guesses or [red]you'll perish[/red]")
+    game()
 
 
 if __name__ == "__main__":
